6ae77c02420c3c11d5179edcaf61817a4684b417
Petr Sigmund <petr.sigmund@unicorn.net>
2022-04-13 19:49:50 +0200

    Zofka kresli 4 domecky

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -4,11 +4,37 @@ import net.sevecek.turtle.engine.*;
 
 public class HlavniProgram {
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
         Turtle zofka;
 
         zofka = new Turtle();
-        zofka.move(40);
+        zofka.setLocation(40, 200);
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 2; j++) {
+                zofka.move(80);
+                zofka.turnRight(90);
+                zofka.move(83);
+                zofka.turnRight(90);
+            }
+            zofka.penUp();
+            zofka.move(80);
+            zofka.penDown();
+            zofka.turnRight(45);
+            zofka.move(60);
+            zofka.turnRight(90);
+            zofka.move(60);
+            zofka.penUp();
+            zofka.turnLeft(45);
+            zofka.move(30);
+            zofka.turnRight(90);
+            zofka.move(80);
+            zofka.turnRight(180);
+            zofka.penDown();
+        }
+
+
+
+
 
     }
 
